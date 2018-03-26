8ce1cb38b6abe8793518c0882f0019480b06b2a6
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-26 16:53:51 +0200

    indente validestap

--- main/validestap.ino
+++ main/validestap.ino
@@ -2,26 +2,26 @@
 
 void validestap(){
    if(steptab[0] == 0){
-    steptab[0] = stapA();
-  }else if(steptab[1] == 0){
-    steptab[1] = stapB();
-  }else if(steptab[2] == 0){
+      steptab[0] = stapA();
+   }else if(steptab[1] == 0){
+      steptab[1] = stapB();
+   }else if(steptab[2] == 0){
 //     steptab[2] = stapC();
-  }
-  int i = -1;
-  while(i < 3){
-  if(steptab[i] == 1){
-   digitalWrite(ledpin[i], HIGH);
-  }else{
-   digitalWrite(ledpin[i], LOW);
-  }
-  ++i;
-  }
-  if(steptab[0] == 1 && steptab[1] == 1 && steptab[2] == 1){
-    while(true){
-      m.clear();
-      m.writeSprite(0, 0, smile01);
-      stepper.step(30024);
+   }
+   int i = -1;
+   while(i < 3){
+     if(steptab[i] == 1){
+     digitalWrite(ledpin[i], HIGH);
+     } else {
+        digitalWrite(ledpin[i], LOW);
+     }
+      ++i;
     }
-  }
+    if(steptab[0] == 1 && steptab[1] == 1 && steptab[2] == 1){
+      while(true){
+        m.clear();
+        m.writeSprite(0, 0, smile01);
+        stepper.step(30024);
+      }
+    }
 }
