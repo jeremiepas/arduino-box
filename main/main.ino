b5ae9e3036a7de68fa7b5ed5537f8285402034a7
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-26 16:53:19 +0200

    indente main.ion

--- main/main.ino
+++ main/main.ino
@@ -8,17 +8,25 @@ int ledpin[3] = {2, 3, 4};
 int steptab[3] = {0,0,1};
 int pinBouton[4] = {8,9,10,11}; //led de test
 int code[4] = {0,0,0,0};
+//-------------------------------------------------------------
 Photocell photocell1(5);
 Photocell photocell2(6);
 Photocell photocell3(7);
+//-------------------------------------------------------------
+/*
+  m is object Ledmatrix use for led ecran
+*/
 int DIN = 19;   // DIN pin of MAX7219 module
 int CLK = 13;   // CLK pin of MAX7219 module
 int CS = 18;    // CS pin of MAX7219 module
 int maxInUse = 1;
 Ledmatrix m(DIN, CS, CLK, maxInUse);
+//-------------------------------------------------------------
+/*
+  stepper object use for control motor stepper for  open the box
+*/
 Stepper stepper (STEPS, 14, 16, 15, 17);
-
-//--------------------------------------------------------------
+//-------------------------------------------------------------
 int secondes_remain = 2;
 int minutes_remain = 1;
 //--------------------------------------------------------------
@@ -38,16 +46,16 @@ char heart[10][6] =
     B00000000,
   },
   { 4, 8,//2
-    B00011101,    
-    B00010101,     
+    B00011101,
+    B00010101,
     B00010111,
     B00000000,
   },
   { 4, 8,//3
-    B00010101,  
-    B00010101,     
+    B00010101,
+    B00010101,
     B00011111,
-    B00000000,  
+    B00000000,
   },
   {4, 8,//4
   B00000111,
@@ -139,42 +147,11 @@ void setup() {
   pinMode(ledpin[0], OUTPUT);
   pinMode(ledpin[1], OUTPUT);
   pinMode(ledpin[2], OUTPUT);
-  //----------------
-//  int i = 0;
-//  while(i++ < 4 ){
-////  pinMode(ledcode[i], OUTPUT);
-//    pinMode(pinBouton[i],INPUT);
-//  }
+  //------------------------------
+
 }
 
 void loop() {
   timer();
   validestap();
-//-------------------------------------------------------------------------------
-  //  Serial.println(photocell1.getphotocellReading());
-
-//-------------------------------------------------------------------------------
-//  m.setDot(6,6,true); 
-//  delay(1000);
-//  m.setDot(6,3,true);
-//  delay(1000);
-//  m.setDot(6,3,true);
-//  delay(1000);
-//  m.clear(); // Clears the display
-//  m.clear();
-
-  // Displaying the character at x,y (upper left corner of the character)  
-//  m.writeSprite(2, 0, B);
-//  delay(1000);
-//  m.writeSprite(0, 0, smile01);
-//  delay(1000);
-  
- 
-  
- 
-//             int n = -1;
-//      while(n++ < 5){
-//  displayled.setRegistry(n, heart[2][n] + heart[2][n] *31);
-//      }
-
 }
